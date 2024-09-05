b71feaa1805e745ab6628e07d5e3c36b12cb1864
Shavidu Keshan <shavidu4321@gmail.com>
2024-09-05 10:29:54 +0530

    create nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,2 +1,20 @@
+import React from 'react';
+import './Nav.css';
 
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <div className="nav-brand">Marlon</div>
+      <ul className="nav-menu">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#work">Work</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
 
